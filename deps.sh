79f6d829c25db30669e50446f96514823faab3cc
Jeremy Soller <jackpot51@gmail.com>
2017-03-20 14:45:25 -0600

    Update to add checkbox default tests

--- deps.sh
+++ deps.sh
@@ -1,4 +1,4 @@
 # Install dependencies, to be moved into debian/control
 sudo add-apt-repository ppa:hardware-certification/public
 sudo apt update
-sudo apt install checkbox-ng
+sudo apt install checkbox-ng plainbox-provider-checkbox
