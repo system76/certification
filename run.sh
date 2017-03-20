c7fb65a3ee02ca6061e314c611fcf9ec669b7521
Jeremy Soller <jackpot51@gmail.com>
2017-03-20 14:38:01 -0600

    Remove text export, rename categories and plan

--- run.sh
+++ run.sh
@@ -5,20 +5,19 @@ cd provider
 ./manage.py develop -f
 cd ..
 
-JOB=automated
-DATE="$(date "+%Y-%m-%d_%H-%M-%S")"
-REPORT="report/$JOB/"
+PLAN=automated
+REPORT="report/$PLAN/"
 mkdir -p "$REPORT"
 cd "$REPORT"
 
-cat << EOF > launcher.conf
+cat > launcher.conf <<EOF
 [launcher]
 launcher_version = 1
 app_id = 2017.com.system76:launcher
 stock_reports = text
 
 [test plan]
-unit = 2017.com.system76::$JOB
+unit = 2017.com.system76::$PLAN
 forced = yes
 
 [test selection]
@@ -44,10 +43,6 @@ path = report.html
 type = file
 path = report.json
 
-[transport:text]
-type = file
-path = report.txt
-
 [report:html]
 exporter = html
 transport = html
@@ -56,11 +51,6 @@ forced = yes
 [report:json]
 exporter = json
 transport = json
-forced = yes
-
-[report:text]
-exporter = text
-transport = text
 forced = yes
 EOF
 
