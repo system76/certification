0ccc242863df21b1e0928ef4ccce631f289b1bc0
Jeremy Soller <jackpot51@gmail.com>
2017-03-20 15:24:08 -0600

    Add more dependencies

--- deps.sh
+++ deps.sh
@@ -1,4 +1,5 @@
 # Install dependencies, to be moved into debian/control
+set -e
 sudo add-apt-repository ppa:hardware-certification/public
 sudo apt update
-sudo apt install checkbox-ng plainbox-provider-checkbox
+sudo apt install checkbox-ng plainbox-provider-checkbox fswebcam fwts iperf
