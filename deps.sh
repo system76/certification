da5ce2b15ce989d1450e1401094c0276bb362c64
Jeremy Soller <jackpot51@gmail.com>
2017-03-20 15:25:11 -0600

    Cleanup deps script

--- deps.sh
+++ deps.sh
@@ -1,5 +1,4 @@
 # Install dependencies, to be moved into debian/control
 set -e
-sudo add-apt-repository ppa:hardware-certification/public
-sudo apt update
+sudo add-apt-repository -u ppa:hardware-certification/public
 sudo apt install checkbox-ng plainbox-provider-checkbox fswebcam fwts iperf
