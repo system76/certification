a4b77bb57f7e96ae2da81ef7d108e2a29fe112b1
Jeremy Soller <jackpot51@gmail.com>
2017-03-24 09:21:11 -0600

    Update dependencies to pull in more tests

--- deps.sh
+++ deps.sh
@@ -1,4 +1,4 @@
 # Install dependencies, to be moved into debian/control
 set -e
 sudo add-apt-repository -u ppa:hardware-certification/public
-sudo apt install checkbox-ng plainbox-provider-checkbox fswebcam fwts iperf
+sudo apt install checkbox-ng plainbox-provider-certification-client
