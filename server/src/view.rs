75fb271f063450eb01d125bce04799ce8c75a190
Jeremy Soller <jackpot51@gmail.com>
2017-04-07 13:31:35 -0600

    Update style, add failed count to test list

--- server/src/view.rs
+++ server/src/view.rs
@@ -1,21 +1,58 @@
 use rocket_contrib::Template;
-use std::io;
+use std::{cmp, io};
 use test::Test;
 use util;
 
 #[get("/view/<model>")]
 fn model(model: &str) -> io::Result<Template> {
     #[derive(Serialize)]
+    struct TestRow {
+        name: String,
+        failed: usize,
+        passed: usize,
+        not_supported: usize,
+        total: usize,
+    }
+
+    #[derive(Serialize)]
     struct Context {
         version: &'static str,
         model: String,
-        tests: Vec<String>,
+        tests: Vec<TestRow>,
+    }
+
+    let mut tests = Vec::new();
+    for test in util::list_tests(model)? {
+        let data = Test::from_str(&util::read_test(model, &test)?)?;
+
+        let mut failed = 0;
+        let mut passed = 0;
+        let mut not_supported = 0;
+        let mut total = 0;
+        if let Some(results) = data.results {
+            for result in results {
+                match result.status.as_str() {
+                    "failed" => failed += 1,
+                    "passed" => passed += 1,
+                    _ => not_supported += 1
+                }
+                total += 1;
+            }
+        }
+
+        tests.push(TestRow {
+            name: test,
+            failed: failed,
+            passed: passed,
+            not_supported: not_supported,
+            total: total,
+        });
     }
 
     Ok(Template::render("view/model", &Context {
         version: util::version(),
         model: model.to_string(),
-        tests: util::list_tests(model)?
+        tests: tests
     }))
 }
 
@@ -29,10 +66,18 @@ fn test(model: &str, test: &str) -> io::Result<Template> {
         data: Test,
     }
 
+    let mut data = Test::from_str(&util::read_test(model, test)?)?;
+    if let Some(ref mut results) = data.results {
+        results.sort_by(|a, b| match a.status.cmp(&b.status) {
+            cmp::Ordering::Equal => a.id.cmp(&b.id),
+            not_equal => not_equal
+        });
+    }
+
     Ok(Template::render("view/test", &Context {
         version: util::version(),
         model: model.to_string(),
         test: test.to_string(),
-        data: Test::from_str(&util::read_test(model, test)?)?
+        data: data
     }))
 }
