94c923cd2a40bf22da294fbf341936b65b0bed9c
Jeremy Soller <jackpot51@gmail.com>
2017-04-07 13:41:46 -0600

    Automate ppa and deps install

--- deps.sh
+++ deps.sh
@@ -1,9 +1,9 @@
 # Install dependencies, to be moved into debian/control
 set -e
 
-sudo add-apt-repository -u ppa:hardware-certification/public
-sudo apt install checkbox-ng checkbox-converged \
-                 plainbox-provider-certification-client \
-                 net-tools
+sudo add-apt-repository -y -u ppa:hardware-certification/public
+sudo apt install -y checkbox-ng checkbox-converged \
+                    plainbox-provider-certification-client \
+                    net-tools
 
 git submodule update --init --remote
