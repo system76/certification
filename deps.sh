38e82ad3404a154e4ad0f789b8d5e76a2c325985
Jeremy Soller <jackpot51@gmail.com>
2017-06-20 14:08:54 -0600

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -1,7 +1,8 @@
 # Install dependencies, to be moved into debian/control
 set -e
 
-sudo add-apt-repository -y -u ppa:hardware-certification/public
+sudo add-apt-repository -y ppa:hardware-certification/public
+sudo apt update -y
 sudo apt install -y checkbox-ng checkbox-converged \
                     plainbox-provider-certification-client \
                     net-tools
