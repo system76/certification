f277630ff2c51054534dcda45b5da0e46cb90598
Jeremy Soller <jeremy@system76.com>
2017-10-23 13:36:43 -0600

    Use plainbox-provider-checkbox for autotesting

--- deps.sh
+++ deps.sh
@@ -5,6 +5,7 @@ sudo add-apt-repository -y ppa:hardware-certification/public
 sudo apt update -y
 sudo apt install -y \
     checkbox-ng \
+    plainbox-provider-checkbox \
     net-tools \
     snapd
 sudo snap install core
